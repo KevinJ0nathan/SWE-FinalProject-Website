5bc36ce44d9b4f8fb8aa221e118aa0b60a9c5a1b
Kevin Jonathan <realkevinjonathan@gmail.com>
2025-12-06 15:36:39 +0700

    added mqtt logic for light state

--- src/components/DashboardLayout.jsx
+++ src/components/DashboardLayout.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import LightModeButton from './LightModeButton'
 import LightSensorSlider from './LightSensorSlider'
 import CameraFeed from './CameraFeed'
@@ -6,7 +6,19 @@ import LightStatus from './LightStatus'
 import AIConfidence from './AIConfidence'
 import LightBrightness from './LightBrightness'
 
+import client from '../mqttClient'
+
 const DashboardLayout = () => {
+  const[lightState, setLightState] = useState("...");
+
+  useEffect(() => {
+    client.on("message", (topic, message) => {
+      if (topic === "light_state") {
+        setLightState(message.toString());
+      }
+    });
+  }, []);
+  
   return (
     <div className='pt-20 pb-8 px-6 max-w-[1600px] mx-auto w-full'>
         <div className='grid grid-cols-1 lg:grid-cols-12 gap-6 pt-10 h-[calc(100vh-8rem)]'>
@@ -14,7 +26,7 @@ const DashboardLayout = () => {
             <div className='lg:col-span-8 h-full'>
                 <CameraFeed isConnected={true}></CameraFeed>
                 <div className='flex gap-5 flex-wrap'>
-                <LightStatus></LightStatus>
+                <LightStatus lightStatus={lightState}></LightStatus>
                 <AIConfidence></AIConfidence>
                 <LightBrightness></LightBrightness>
                 </div>
---
+++ src/mqttClient.js
@@ -0,0 +1,23 @@
+import mqtt from "mqtt";
+
+let client;
+
+function createClient() {
+  if (!client) {
+    client = mqtt.connect("ws://localhost:9001", {
+      clientId: "react_" + Math.random().toString(16).substr(2, 8),
+    });
+
+    client.on("connect", () => {
+      console.log("MQTT Connected");
+      client.subscribe("light_state");
+    });
+
+    client.on("error", (err) => console.log("MQTT Error:", err));
+    client.on("close", () => console.log("MQTT disconnected"));
+  }
+
+  return client;
+}
+
+export default createClient();
